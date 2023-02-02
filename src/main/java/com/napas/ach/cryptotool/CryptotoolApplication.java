56ebff1606820d4f4bd8985b80f26269c62c6be4
Nguyen Xuan Huy <huynx@napas.com.vn>
2023-02-02 17:01:58 +0700

    Khởi tạo

---
+++ src/main/java/com/napas/ach/cryptotool/CryptotoolApplication.java
@@ -0,0 +1,13 @@
+package com.napas.ach.cryptotool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotoolApplication.class, args);
+	}
+
+}
